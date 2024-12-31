8bf96b60e5e76f5e0df60c2a4c2c26661020e614
yahyahudan19 <yahyahudan19@gmail.com>
2024-12-31 14:45:01 +0700

    Layouting Template

--- routes/web.php
+++ routes/web.php
@@ -12,10 +12,17 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/template', function () {
+    return view('layouts.template');
+})->middleware(['auth', 'verified'])->name('template');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Global Routes
+
 });
 
 require __DIR__.'/auth.php';
